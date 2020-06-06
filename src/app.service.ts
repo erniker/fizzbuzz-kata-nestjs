618872d78ff38916f9df84e2e0d23164f2dfb4f9
erniker <josepablomhotmail.comedinagrande@>
2020-06-06 13:00:26 +0200

    Add possibility to configure fizz buzz numbers

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,13 @@ import { FizzBuzz } from './FizzBuzz';
 @Injectable()
 export class AppService {
   constructor(private FizzBuzz: FizzBuzz) {}
-  generateFizzBuzzList(numbers: number[]): (number | string)[] {
-    return numbers.map(number => this.FizzBuzz.getFizzBuzz(number));
+  generateFizzBuzzList(
+    numbers: number[],
+    fizzNumber: number,
+    buzzNumber: number,
+  ): (number | string)[] {
+    return numbers.map(number =>
+      this.FizzBuzz.getFizzBuzz(number, fizzNumber, buzzNumber),
+    );
   }
 }
