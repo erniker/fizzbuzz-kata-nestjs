4f870d8bdcbc6f8cbb931db0d57ba8fb1dccfe07
erniker <josepablomhotmail.comedinagrande@>
2020-06-06 13:25:21 +0200

    add const fizzNumber buzzNumber to test

--- src/app.service.spec.ts
+++ src/app.service.spec.ts
@@ -11,25 +11,28 @@ describe('appService', () => {
     }).compile();
     appService = await module.get<AppService>(AppService);
   });
+
+  const fizzNumber = 3;
+  const buzzNumber = 4;
+
   it('Given a list of numbers with no match in Fizz or Buzz, return a list of numbers', () => {
-    expect(appService.generateFizzBuzzList([1, 7], 3, 4)).toStrictEqual([1, 7]);
+    expect(
+      appService.generateFizzBuzzList([1, 7], fizzNumber, buzzNumber),
+    ).toStrictEqual([1, 7]);
   });
   it('Given a list of numbers with match in Fizz, return the same list of numbers replacing the matchs with Fizz', () => {
-    expect(appService.generateFizzBuzzList([1, 3], 3, 4)).toStrictEqual([
-      1,
-      'Fizz',
-    ]);
+    expect(
+      appService.generateFizzBuzzList([1, 3], fizzNumber, buzzNumber),
+    ).toStrictEqual([1, 'Fizz']);
   });
   it('Given a list of numbers with match in Buzz, return the same list of numbers replacing the matchs with Buzz', () => {
-    expect(appService.generateFizzBuzzList([1, 4], 3, 4)).toStrictEqual([
-      1,
-      'Buzz',
-    ]);
+    expect(
+      appService.generateFizzBuzzList([1, 4], fizzNumber, buzzNumber),
+    ).toStrictEqual([1, 'Buzz']);
   });
   it('Given a list of numbers with match in FizzBuzz, return the same list of numbers replacing the matchs with FizzBuzz', () => {
-    expect(appService.generateFizzBuzzList([2, 12], 3, 4)).toStrictEqual([
-      2,
-      'FizzBuzz',
-    ]);
+    expect(
+      appService.generateFizzBuzzList([2, 12], fizzNumber, buzzNumber),
+    ).toStrictEqual([2, 'FizzBuzz']);
   });
 });
